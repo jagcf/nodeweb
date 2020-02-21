776b89f18e98740be39157ecc9809525a4a6aaa7
Jag CF <59068693+jagcf@users.noreply.github.com>
2020-02-20 17:00:33 -0800

    Update meetupdemo.js

--- meetupdemo.js
+++ meetupdemo.js
@@ -22,7 +22,7 @@ app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-M
 // routes ======================================================================
 // application -------------------------------------------------------------
 app.get('*', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile(__dirname + '/public/index2.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 // listen (start app with node server.js) ======================================
